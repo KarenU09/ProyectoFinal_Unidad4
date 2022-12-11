642eb32b4e10a60f44b6755ab7543da51cd1c31d
KarenU09 <karenu91@gmail.com>
2022-12-10 18:31:46 -0600

    Línea de conexion creada

---
+++ ProyectoFinal_Unidad4/conexion.vb
@@ -0,0 +1,21 @@
+﻿Imports MySql.Data.MySqlClient
+Imports System.Data
+
+
+Public Class conexion
+
+    Public cmd As New MySqlCommand
+    Public da As New MySqlDataAdapter
+    Public dt As New DataTable
+    Public result As String
+    Public Function strconexion() As MySqlConnection
+        Return New MySqlConnection("server=localhot; user id=ugb; pasword=ugb2022; database=academica; port=3606")
+
+
+    End Function
+
+    Public strcon As MySqlConnection = strconexion()
+
+
+
+End Class
